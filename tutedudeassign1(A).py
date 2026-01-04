2813f0e2c1dff30d6bba1c64b39cd3728fc3295e
Abhishek Deep <deepabhishek50@gmail.com>
2026-01-04 23:57:00 +0530

    Add files via upload
    These are the 2 python files I have submitted here for the python course I am doing on tutedude by Manish Sharma

---
+++ tutedudeassign1(A).py
@@ -0,0 +1,16 @@
+#entering two numbers and printing them
+num1 = 6
+num2 = 3
+print("Enter the first number : ", num1)
+print("Enter the second number :", num2)
+
+#performing mathematical operations using arithmetic operators
+
+Addition = num1 + num2
+print("Addition : ", Addition)
+
+Subtraction = num1 - num2
+print("Subtraction : ", Subtraction)
+
+Multiplication = num1 * num2
+print("Multiplication : ", Multiplication)
---
+++ tutedudeassign1(B).py
@@ -0,0 +1,8 @@
+first_name = input("Enter your first name : ")
+# printing the first name  you enter
+
+last_name = input("Enter your last name : ")
+# printing the second name you enter
+
+print("Hello," + " " + first_name + " " + last_name + "!" + " " + "Welcome to the Python program.")
+

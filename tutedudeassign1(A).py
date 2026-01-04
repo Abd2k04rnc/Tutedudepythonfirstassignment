b024b53f28c4dbfca88c95edf5a078c2e85ce74c
Abhishek Deep <deepabhishek50@gmail.com>
2026-01-05 00:06:49 +0530

    Add division operation to arithmetic calculations

--- tutedudeassign1(A).py
+++ tutedudeassign1(A).py
@@ -11,6 +11,7 @@ print("Addition : ", Addition)
 
 Subtraction = num1 - num2
 print("Subtraction : ", Subtraction)
-
 Multiplication = num1 * num2
 print("Multiplication : ", Multiplication)
+Division = num1 / num2
+print("Division : ", Division)
